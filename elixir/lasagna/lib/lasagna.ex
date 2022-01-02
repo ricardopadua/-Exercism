fd731446bf004341f64821b1b73bd071161134b8
Ricardo Pádua <ricardo.padua4@gmail.com>
2022-01-01 23:54:38 -0300

    lasagna exercise

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,11 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven, do: 40
+
+  def remaining_minutes_in_oven(minutes), do: expected_minutes_in_oven() - minutes
+
+  # Please define the 'preparation_time_in_minutes/1' function
+
+  # Please define the 'total_time_in_minutes/2' function
+
+  # Please define the 'alarm/0' function
+end
