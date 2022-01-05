90c5a0083f814a150d148297fbb722f0b33604e4
Ricardo Pádua <ricardo.padua4@gmail.com>
2022-01-04 22:50:01 -0300

    feat(freelancer-rates) Calculate the monthly rate, given an hourly rate and a discount

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,15 +1,18 @@
 defmodule FreelancerRates do
   @hourly_rate_times 8
+  @month_billable_days 22
 
   def daily_rate(hourly_rate), do: hourly_rate * @hourly_rate_times / 1
 
   def apply_discount(before_discount, discount), do: before_discount - (before_discount / 100) * discount
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    monthly_rate = daily_rate(hourly_rate) * @month_billable_days
+    monthly_rate - ((monthly_rate / 100) * discount)
+    |> Float.ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+
   end
 end
