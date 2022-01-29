0a6437bee86a5079db5d280977752323ddaf5734
Ricardo Padua <ricardo.padua4@gmail.com>
2022-01-29 10:01:21 -0300

    Define if Pac-Man scores

--- elixir/pacman-rules/lib/rules.ex
+++ elixir/pacman-rules/lib/rules.ex
@@ -1,10 +1,10 @@
 defmodule Rules do
   def eat_ghost?(power_pellet_active, touching_ghost) do
-    # Please implement the eat_ghost?/2 function
+    power_pellet_active and touching_ghost
   end
 
   def score?(touching_power_pellet, touching_dot) do
-    # Please implement the score?/2 function
+    touching_power_pellet or touching_dot
   end
 
   def lose?(power_pellet_active, touching_ghost) do
