b2914843ef0fc78c178057f4880a392018f78235
Ricardo Padua <ricardo.padua4@gmail.com>
2022-01-30 01:36:14 -0300

    Send an alert

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -2,10 +2,10 @@ defmodule LogLevel do
   def to_label(level, legacy?) do
    cond do
       {0, false} == {level, legacy?} -> :trace
-      {1, true} == {level, legacy?} -> :debug
-      {2, true} == {level, legacy?} -> :info
-      {3, true} == {level, legacy?} -> :warning
-      {4, true} == {level, legacy?} -> :error
+      {1} == {level} -> :debug
+      {2} == {level} -> :info
+      {3} == {level} -> :warning
+      {4} == {level} -> :error
       {5, false} == {level, legacy?} -> :fatal
       true -> :unknown
     end
@@ -14,8 +14,8 @@ defmodule LogLevel do
   def alert_recipient(level, legacy?) do
     label = to_label(level, legacy?)
     cond do
-      {label, legacy?} == {:error, true} -> :ops
-      {label, legacy?} == {:fatal, true} -> :ops
+      {label} == {:error} -> :ops
+      {label} == {:fatal} -> :ops
       {label, legacy?} == {:unknown, true} -> :dev1
       {label, legacy?} == {:unknown, false} -> :dev2
       true -> false
